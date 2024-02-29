aa04bdb018c38c4bc75b546a2c6d6d95b4d92a73
sjake12 <134672864+sjake12@users.noreply.github.com>
2024-02-29 10:15:06 +0800

    Add files via upload

---
+++ Activity1/src/AverageGrade.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ */
+public class AverageGrade {
+    
+    public static String calculateEquivalent(double average){
+        if(average > 99){
+            return "1.00";
+        }else if(average >= 96 && average <= 98){
+            return "1.25";
+        }else if(average >= 93 && average <= 95){
+            return "1.50";
+        }else if(average >= 90 && average <= 92){
+            return "1.75";
+        }else if(average >= 87 && average <= 89){
+            return "2.00";
+        }else if(average >= 84 && average <= 86){
+            return "2.25";
+        }else if(average >= 81 && average <= 83){
+            return "2.50";
+        }else if(average >= 78 && average <= 80){
+            return "2.75";
+        }else if(average >= 75 && average <= 77){
+            return "3.00";
+        }else{
+            return "FAILED";
+        }
+    }
+    
+    public static void main (String[] args){
+        Scanner myObj = new Scanner(System.in);
+        String[] subjects = {"Algorithms and Complexity(AC221)", "Information Management(CC115)", "Science, Technology and Society(GE704)",
+            "The life and Works of Jose Rizal(GE709)", "Networks and Communications(NC225)", "Operating Systems(OS223)",
+            "Team Sports(PE104)", "Programming Languages(PL222)"};
+        int noOfSubjects = subjects.length;
+        int sum = 0;
+         
+        
+        System.out.println("--------------Average grade calculator---------------------");
+        
+        for(int i = 0; i < noOfSubjects; i++){
+            System.out.print("Enter grade for " + subjects[i] + ": ");
+            int grade = myObj.nextInt();
+            sum += grade;
+        }
+        
+        double average = sum / noOfSubjects;
+        String equivalent = calculateEquivalent(average);
+        
+        System.out.println("\nAverage grade is: " + average);
+        System.out.println("Equivalent: " + equivalent);
+    }
+}
